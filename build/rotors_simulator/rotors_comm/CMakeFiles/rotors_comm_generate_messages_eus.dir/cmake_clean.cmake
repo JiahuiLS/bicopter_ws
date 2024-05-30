8f009b0816e5df5633e2a5e2e3e050f0bdeec994
Rio Hui <2021105406@qq.com>
2024-05-30 16:59:29 +0800

    git init

---
+++ build/rotors_simulator/rotors_comm/CMakeFiles/rotors_comm_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/hui/bicopter_ws/devel/share/roseus/ros/rotors_comm/manifest.l"
+  "/home/hui/bicopter_ws/devel/share/roseus/ros/rotors_comm/msg/WindSpeed.l"
+  "/home/hui/bicopter_ws/devel/share/roseus/ros/rotors_comm/srv/Octomap.l"
+  "/home/hui/bicopter_ws/devel/share/roseus/ros/rotors_comm/srv/RecordRosbag.l"
+  "CMakeFiles/rotors_comm_generate_messages_eus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/rotors_comm_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
